d4b7311d98f06404f190bf1974c582c2fd28a98e
eliran <mma911615@gmail.com>
2026-03-31 22:58:54 +0300

    adding electron support

---
+++ main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow, Menu } from 'electron/main'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile(
+    path.join(__dirname, './dist/ai-chat/browser/index.html')
+  );
+}
+
+app.whenReady().then(() => {
+  //Menu.setApplicationMenu(null);
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
