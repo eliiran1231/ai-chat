85ed40e234b3a80552f1a975e5b4dfb6fc2edd8a
eliran <mma911615@gmail.com>
2026-04-16 19:13:30 +0300

    making last message a property

--- src/classes/Agent.ts
+++ src/classes/Agent.ts
@@ -1,26 +1,28 @@
 import { Answer } from "./Answer";
 import { Chat } from "./Chat";
+import { Message } from "./Message";
 import { Question } from "./Question";
 import { Supporter } from "./Supporter";
 
 export class Agent{
     chat: Chat = null as any;
     supporter: Supporter = new Supporter();
-    lastQuestion: Question | undefined;
+    lastQuestion?: Question;
+    lastMessage?: Message;
     init(chat : Chat, supporter : Supporter) {
         this.chat = chat;
         this.supporter = supporter;
     }
     async respond(): Promise<void> {
-        const lastMessage = this.chat.messages.at(-1);
-        if (!lastMessage) {
+        this.lastMessage = this.chat.messages.at(-1);
+        if (!this.lastMessage) {
             throw new Error("respond was called but there is nothing to respond to");
         }
-        else if(lastMessage.from == "supporter"){
+        else if(this.lastMessage.from == "supporter"){
             throw new Error("respond was called but there is nothing to respond to. the last message is from the agent");
         }
 
-        if(this.lastQuestion && lastMessage instanceof Answer && !this.lastQuestion?.isAnswerValid(lastMessage)){
+        if(this.lastQuestion && this.lastMessage instanceof Answer && !this.lastQuestion?.isAnswerValid(this.lastMessage)){
             this.supporter.sendMessage(this.lastQuestion.validationErrorMessage);
             throw new Error("validation didnt pass");
         }
