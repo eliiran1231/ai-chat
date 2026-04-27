3d7406e072ffd5e43da5961d231fcb221725bed3
eliran <mma911615@gmail.com>
2026-04-27 20:53:22 +0300

    bugfixes

--- src/classes/Message.ts
+++ src/classes/Message.ts
@@ -1,11 +1,12 @@
 export type MessageSender = 'client' | 'supporter';
 export type MessageType = 'message' | 'question' | 'answer';
 export type Attachment = { 
-        type: string,
-        url: string, size: number,
-        extension: string,
-        name: string 
-    };
+    type: string,
+    url: string,
+    size: number,
+    extension: string,
+    name: string 
+};
 
 export class Message {
     id?: number;
