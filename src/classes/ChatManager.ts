3a6fd537b763675bf9159b938fe4d7acb097054a
eliran <mma911615@gmail.com>
2026-06-13 16:13:00 +0300

    creating chat manager service

--- src/classes/ChatManager.ts
+++ src/classes/ChatManager.ts
@@ -1,10 +1,25 @@
 import { Injector } from "@angular/core";
 import { Message } from "./Message";
 import { Chat } from "./Chat";
+import { ChatManagersService } from "../services/chat-managers.service";
 
 export class ChatManager {
     protected chat!: Chat;
+    private _name?: string;
+    private chatManagersService: ChatManagersService;
+
     constructor(protected injector: Injector) {
+        this.chatManagersService = injector.get(ChatManagersService);
+    }
+
+    set name(name: string) {
+        if (this._name) throw new Error("this chat manager name was already set and cannot be changed");
+        this._name = name;
+    }
+
+    get name(): string {
+        if (!this._name) this._name = this.chatManagersService.getManagerName(this);
+        return this._name;
     }
 
     init(chat: Chat): void | Promise<void>{
