f81bfa3f5262b722b7893e863d76e601bdf1d182
NatanAnter <113854055+NatanAnter@users.noreply.github.com>
2026-06-29 13:28:18 +0300

    create costume LucideIconData: CheckCheck (#68)
    * create costume LucideIconData: CheckCheck
    
    * Remove comments from check-check icon paths
    
    Removed Hebrew comments from the check-check icon paths.
    
    * delete package lock

---
+++ src/assets/icons/check-check.ts
@@ -0,0 +1,20 @@
+import type { LucideIconData } from '@lucide/angular';
+
+export const checkcheck: LucideIconData = {
+  name: 'checkcheck',
+  size: 24,
+  node: [
+    [
+      'path',
+      {
+        d : 'M 2.6 13.31 L 6.76 17.47 L 15.56 6.59',
+      },
+    ],
+    [
+      'path',
+      {
+        d: 'M 11.24 16.11 L 12.6 17.47 L 21.4 6.59',
+      },
+    ],
+  ],
+};
